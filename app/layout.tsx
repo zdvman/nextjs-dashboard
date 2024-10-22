a7590511235bef4383803b75fcb209256eed1bf1
Dmytro Zuiev <zuyevdmytro@gmail.com>
2024-10-22 22:07:43 +0100

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+// layout.tsx is a layout component that wraps the entire application. It is used to provide a consistent layout across all pages.
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts"; // Import the Inter font from app/ui/fonts.ts
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* The layout component uses the Inter font from Google Fonts and applies */}
+      {/* it to the body element. */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
